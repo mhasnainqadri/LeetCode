07886ee84047841d4e3852a23205f883ae68e479
mhasnainqadri <hasnainqadri9c@gmail.com>
2026-03-05 00:29:53 +0500

    leetcode

---
+++ Easy/PalindromeNumber.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+class Solution
+{
+public:
+    bool isPalindrome(int num)
+    {
+        long long reversed=0;
+        int copy=num;
+        while(copy>0)
+        {
+            reversed=reversed*10+copy%10;
+            copy/=10;
+        }
+        if(reversed==num) return true;
+        return false;
+    }
+};
+int main()
+{
+    Solution s;
+    (s.isPalindrome(123))?cout<<"true":cout<<"false";
+    return 0;
+}
