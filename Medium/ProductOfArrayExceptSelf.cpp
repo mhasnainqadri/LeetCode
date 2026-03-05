2c14e75cf7238130346fcb0a06fdb9a0cde9dd77
mhasnainqadri <hasnainqadri9c@gmail.com>
2026-03-05 22:16:34 +0500

    leetcode

--- Medium/ProductOfArrayExceptSelf.cpp
+++ Medium/ProductOfArrayExceptSelf.cpp
@@ -6,15 +6,17 @@ class Solution
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
         vector<int> ans(nums.size());
-        for(int i=0,left=1;i<nums.size();i++)
+        int product=1;
+        for(int i=0;i<nums.size();i++)
         {
-            ans[i]=left;
-            left*=nums[i];
+            ans[i]=product;
+            product*=nums[i];
         }
-        for(int i=nums.size()-1,right=1;i>=0;i--)
+        product=1;
+        for(int i=nums.size()-1;i>=0;i--)
         {
-            ans[i]*=right;
-            right*=nums[i];
+            ans[i]*=product;
+            product*=nums[i];
         }
         return ans;
     }
