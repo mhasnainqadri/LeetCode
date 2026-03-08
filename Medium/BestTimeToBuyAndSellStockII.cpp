1ceccff88f1e644f5e6edcffdc1ad0136f96ee34
mhasnainqadri <hasnainqadri9c@gmail.com>
2026-03-08 15:59:12 +0500

    leetcode

--- Medium/BestTimeToBuyAndSellStockII.cpp
+++ Medium/BestTimeToBuyAndSellStockII.cpp
@@ -5,19 +5,19 @@ class Solution
 {
 public:
     int maxProfit(vector<int>& prices) {
-        int min=prices[0];
+        int minPrice=prices[0];
         int profit=0;
         for(int i=1;i<prices.size();i++)
         {
-            if(prices[i]<min) min=prices[i];
+            if(prices[i]<minPrice) minPrice=prices[i];
             if(i<prices.size()-1&&prices[i]>prices[i+1])
             {
-                profit+=prices[i]-min;
-                min=prices[i];
+                profit+=prices[i]-minPrice;
+                minPrice=prices[i];
             }
             if(i==prices.size()-1)
             {
-                profit+=prices[i]-min;
+                profit+=prices[i]-minPrice;
             }
         }
         return profit;
